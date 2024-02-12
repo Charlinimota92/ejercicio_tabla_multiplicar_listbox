45a86e086f383024445ef85e70e3e3b5b66a9d40
heurilinares <heurilinares@DESKTOP-7F9G3G4>
2024-02-11 19:04:16 -1200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ejercicio_tabla_multiplicar_listbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txt_Numero_Tabla = new System.Windows.Forms.TextBox();
+            this.btn_Agregar = new System.Windows.Forms.Button();
+            this.btn_Borrar = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(302, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número de la tabla";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(91, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(155, 199);
+            this.listBox1.TabIndex = 1;
+            // 
+            // txt_Numero_Tabla
+            // 
+            this.txt_Numero_Tabla.Location = new System.Drawing.Point(463, 65);
+            this.txt_Numero_Tabla.Name = "txt_Numero_Tabla";
+            this.txt_Numero_Tabla.Size = new System.Drawing.Size(100, 20);
+            this.txt_Numero_Tabla.TabIndex = 2;
+            // 
+            // btn_Agregar
+            // 
+            this.btn_Agregar.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Agregar.Location = new System.Drawing.Point(463, 136);
+            this.btn_Agregar.Name = "btn_Agregar";
+            this.btn_Agregar.Size = new System.Drawing.Size(100, 66);
+            this.btn_Agregar.TabIndex = 3;
+            this.btn_Agregar.Text = "Calcular";
+            this.btn_Agregar.UseVisualStyleBackColor = true;
+            this.btn_Agregar.Click += new System.EventHandler(this.btn_Agregar_Click);
+            // 
+            // btn_Borrar
+            // 
+            this.btn_Borrar.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Borrar.Location = new System.Drawing.Point(463, 255);
+            this.btn_Borrar.Name = "btn_Borrar";
+            this.btn_Borrar.Size = new System.Drawing.Size(100, 58);
+            this.btn_Borrar.TabIndex = 4;
+            this.btn_Borrar.Text = "Borrar";
+            this.btn_Borrar.UseVisualStyleBackColor = true;
+            this.btn_Borrar.Click += new System.EventHandler(this.btn_Borrar_Click);
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Salir.Location = new System.Drawing.Point(157, 320);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(111, 59);
+            this.btn_Salir.TabIndex = 6;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = true;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 450);
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.btn_Borrar);
+            this.Controls.Add(this.btn_Agregar);
+            this.Controls.Add(this.txt_Numero_Tabla);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txt_Numero_Tabla;
+        private System.Windows.Forms.Button btn_Agregar;
+        private System.Windows.Forms.Button btn_Borrar;
+        private System.Windows.Forms.Button btn_Salir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio_tabla_multiplicar_listbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Borrar_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            txt_Numero_Tabla.Clear();
+        }
+
+        private void btn_Agregar_Click(object sender, EventArgs e)
+        {
+            int numero, tabla, resultado;
+
+            tabla = int.Parse(txt_Numero_Tabla.Text);
+
+            for(numero = 0; numero <=12; numero++)
+            {
+                resultado = numero * tabla;
+                listBox1.Items.Add(tabla + "x" + numero + "=" + resultado);
+
+            }
+        }
+    }
+}
